04a10c02b08672e0df4b8a94e5a8cae0a23f7696
C Houghton <cscjh@it000441.users.bris.ac.uk>
2018-05-18 17:03:55 +0100

    minor addition

---
+++ eeg/make_event_vectors.jl
@@ -0,0 +1,11 @@
+
+function make_event_vectors()
+
+    function get_rand()
+        rand(-1.5:0.5:1.5)
+    end
+
+    events=Float64[2.0+get_rand(),-3.0+get_rand(),3.0+get_rand(),2.0+get_rand(),5.0+get_rand()]
+
+    
+    
